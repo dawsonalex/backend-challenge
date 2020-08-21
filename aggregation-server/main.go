87fb5f31d289c9cbe54f93ee298c7a2219a95d59
Alexander Dawson <alexander_dawson@ymail.com>
2020-08-21 12:07:39 +0100

    Setup http server and graceful shutdown from main

--- aggregation-server/main.go
+++ aggregation-server/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/dawsonalex/aggregator/watcher"
 
@@ -18,4 +21,36 @@ func main() {
 	mux.HandleFunc("/hello", server.HelloHandler(reg))
 	mux.HandleFunc("/bye", server.ByeHandler(reg))
 	mux.HandleFunc("files", server.FilesHandler())
+
+	srv := &http.Server{Addr: ":8000"}
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	// Wait here until SIGINT received, then exec callback function
+	// to gracefully shutdown.
+	awaitInterrupt(func(done chan bool) {
+		if err := srv.Shutdown(context.TODO()); err != nil {
+			panic(err)
+		}
+		done <- true
+	})
+	log.Info("Aggregator stopped.")
+}
+
+func awaitInterrupt(onInterrupt func(chan bool)) {
+	done := make(chan bool)
+	go func() {
+		// Wait for SIGINT to stop services.
+		sigchan := make(chan os.Signal, 1)
+		signal.Notify(sigchan, os.Interrupt)
+		defer signal.Stop(sigchan)
+		<-sigchan
+
+		go onInterrupt(done)
+	}()
+
+	<-done
 }
