c8140487f721f2eafcbb684114f3808e347fbfb9
Alexander Dawson <alexander_dawson@ymail.com>
2020-08-21 10:53:09 +0100

    Implement AddNode functionality for registering nodes.

--- aggregation-server/main.go
+++ aggregation-server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"github.com/dawsonalex/aggregator/watcher"
+
 	"github.com/dawsonalex/aggregator/server"
 	log "github.com/sirupsen/logrus"
 )
@@ -10,8 +12,10 @@ import (
 func main() {
 	log.Info("Starting aggregator")
 
+	reg := watcher.NewRegistry()
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", server.HelloHandler())
-	mux.HandleFunc("/bye", server.ByeHandler())
+	mux.HandleFunc("/hello", server.HelloHandler(reg))
+	mux.HandleFunc("/bye", server.ByeHandler(reg))
 	mux.HandleFunc("files", server.FilesHandler())
 }
--- aggregation-server/server/server.go
+++ aggregation-server/server/server.go
@@ -1,18 +1,43 @@
 package server
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/dawsonalex/aggregator/watcher"
+	log "github.com/sirupsen/logrus"
 )
 
 // HelloHandler handles requests to the /hello endpoint.
-func HelloHandler() http.HandlerFunc {
+func HelloHandler(reg *watcher.Registry) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !(r.Method == http.MethodPost) {
+			log.Errorf("Invalid HTTP method, got: %v", r.Method)
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Errorf("Error reading body: %v", err)
+			http.Error(w, "Error reading body", http.StatusBadRequest)
+			return
+		}
 
+		var node watcher.Node
+		err = json.Unmarshal(body, &node)
+		if err != nil {
+			log.Errorf("Error unmarshalling node: %v", err)
+			http.Error(w, "Error parding JSON", http.StatusBadRequest)
+			return
+		}
+
+		reg.AddNode(&node)
 	})
 }
 
 // ByeHandler handles requests to the /bye endpoint.
-func ByeHandler() http.HandlerFunc {
+func ByeHandler(reg *watcher.Registry) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	})
